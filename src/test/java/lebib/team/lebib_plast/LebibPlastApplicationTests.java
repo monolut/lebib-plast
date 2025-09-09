cf31afa07bccfb1c59dce2fd18d5660ecdfc8a5c
monolut <nicolaiashirov@gmail.com>
2025-09-09 19:10:06 +0300

    Add files via upload

---
+++ src/test/java/lebib/team/lebib_plast/LebibPlastApplicationTests.java
@@ -0,0 +1,13 @@
+package lebib.team.lebib_plast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LebibPlastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
